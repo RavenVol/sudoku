e8955a4a9dff685b3090c67bddd718e2acb7228f
RavenVol <r.volianiuk@gmail.com>
2019-08-29 14:39:20 +0300

    Updates

---
+++ precache-manifest.0c779e5aee23e889d4a603980ce403ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d394bf9946197872fba2e9bb4f27c2ed",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "36142c756e67d9d90fea",
+    "url": "/sudoku/static/css/main.87563b8b.chunk.css"
+  },
+  {
+    "revision": "391ede04d1dfe7e38eaf",
+    "url": "/sudoku/static/js/2.e1b64dd7.chunk.js"
+  },
+  {
+    "revision": "36142c756e67d9d90fea",
+    "url": "/sudoku/static/js/main.4829112b.chunk.js"
+  },
+  {
+    "revision": "2376657f656b447c3d4b",
+    "url": "/sudoku/static/js/runtime~main.e7756cf6.js"
+  }
+]);
